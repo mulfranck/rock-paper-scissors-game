2c65d902c1c1ec97f233eef898a42967ad690d90
mulfranck <mulfranck5@gmail.com>
2021-11-16 00:19:00 +0100

    Game on click
    After day 3 of thinking how, finally i can use the users click as a result input not just console.log it;

--- game-script.js
+++ game-script.js
@@ -4,46 +4,33 @@ let playerScore = 0;
 let total = 0;
 let tie = 0;
 
+let playerSelection = ""
+let btns = document.querySelectorAll('.btn');
+
 function computerPlay(){
   let picked = Math.floor((Math.random()*3));
   let choose = ['rock','scissor','paper'];
   return(choose[picked]);
 }
 
-function playerSelection() {
-  let choose;
-  // = prompt("Enter a valid option [Paper, Rock or Scissor] :", "enter") || "";
-  let option = ['rock','paper','scissor'];
-
-  do {
-    choose = prompt("Your Turn enter your choice [Paper, Rock or Scissor] : ")
-    // console.log(choose)
-  } while(!option.includes(choose.toLowerCase()))
-  // while choose is not included in option
-  return choose.toLowerCase();
-}
-
-
-
-
-function playARound (computerSelection, playerSelection){
+function playARound (playerSelection, computerSelection){
   let winner = "";
 
-  console.log(computerSelection, playerSelection)
+  // console.log(typeof playerSelection)
   
 
   //player wins
   if((computerSelection === 'rock' && playerSelection === 'paper') || //paper covers
      (computerSelection === 'paper' && playerSelection === 'scissor') || //scissor cuts
      (computerSelection === 'scissor') && playerSelection === 'rock') { //rock breaks
-        winner = "player";
+        winner = `You win, ${playerSelection} beats ${computerSelection}`;
         ++playerScore;
   }
   //computer wins 
   if((computerSelection === 'paper' && playerSelection === 'rock') || //paper covers
      (computerSelection === 'scissor' && playerSelection === 'paper') || //scissor cuts
      (computerSelection === 'rock') && playerSelection === 'scissor') { //rock breaks
-        winner = "computer";
+        winner = "You Lost!";
         ++computerScore;
   }
 
@@ -55,24 +42,27 @@ function playARound (computerSelection, playerSelection){
   console.log(winner);
 }
 
-
-
-// console.log(playARound(computerPlay(), playerSelection()))
-
-function game () {
-  for (let i = 1; i<=5; i++){
-    playARound(computerPlay(), playerSelection());
-    ++total;
+const endGame = () => {
+  if (playerScore === 5 || computerScore === 5) {
+    gameOver();
   }
-
-  gameOver()
-  
 }
 
-gameOver = () => {
+const gameOver = () => {
   console.log(`
   computer ${computerScore} vs you ${playerScore}
   total runs ${total} and ${tie} ties`)
   console.log(playerScore>computerScore ? "You Win dude! ;D" : "You Lose dude :C")
 }
-game();
+
+btns.forEach(btn => {
+  btn.addEventListener('click', function(e){
+     playerSelection = e.target.textContent;
+     computerSelection = computerPlay();
+
+
+
+    playARound((playerSelection).toLowerCase(), computerSelection)
+    endGame();
+  })
+})
