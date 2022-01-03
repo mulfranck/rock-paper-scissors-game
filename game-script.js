e7c21dc88dd35fa7517116f490f7f1902b3774a7
mulfranck <mulfranck5@gmail.com>
2022-01-03 12:12:53 +0100

    Commented lines used for testing removed, the variable holding the html element prefixed with "$". so to stand out from the other variables.

--- game-script.js
+++ game-script.js
@@ -3,12 +3,12 @@ let computerScore = 0;
 let playerScore = 0;
 let total = 0;
 let tie = 0;
-
 let playerSelection = ""
-let btns = document.querySelectorAll('.btn');
-let playGround = document.querySelector('.playGround');
-let playerScoreHolder = document.querySelector('#player-score');
-let computerScoreHolder = document.querySelector('#computer-score');
+
+let $btns = document.querySelectorAll('.btn');
+let $playGround = document.querySelector('.playGround');
+let $playerScoreHolder = document.querySelector('#player-score');
+let $computerScoreHolder = document.querySelector('#computer-score');
 
 function computerPlay(){
   let picked = Math.floor((Math.random()*3));
@@ -16,33 +16,24 @@ function computerPlay(){
   return(choose[picked]);
 }
 
-function playARound (playerSelection, computerSelection){
-  // let winner = "";
-
-  // console.log(typeof playerSelection)
-  
+function playARound(playerSelection, computerSelection){
   ++total;
   //player wins
   if((computerSelection === 'rock' && playerSelection === 'paper') || //paper covers
      (computerSelection === 'paper' && playerSelection === 'scissor') || //scissor cuts
      (computerSelection === 'scissor') && playerSelection === 'rock') { //rock breaks
-        // winner = `You win, ${playerSelection} beats ${computerSelection}`;
         ++playerScore;
   }
   //computer wins 
   if((computerSelection === 'paper' && playerSelection === 'rock') || //paper covers
      (computerSelection === 'scissor' && playerSelection === 'paper') || //scissor cuts
      (computerSelection === 'rock') && playerSelection === 'scissor') { //rock breaks
-        // winner = "You Lost!";
         ++computerScore;
   }
 
   if (computerSelection == playerSelection){
-    ++tie;  
-    console.log(`tie is ${tie}`)
-    // playARound(computerSelection, playerSelection);
+    ++tie;
   }
-  // console.log(winner);
 }
 
 const endGame = () => {
@@ -56,26 +47,21 @@ const endGame = () => {
 }
 
 function updateScore() {
-  console.log(playerScore)
-  computerScoreHolder.textContent = computerScore;
-  playerScoreHolder.textContent = playerScore;
+  $computerScoreHolder.textContent = computerScore;
+  $playerScoreHolder.textContent = playerScore;
 }
 
 const gameOver = (byWinner) => {
-  // playGround.classList.toggle('mute');
-  playGround.innerHTML += `
+  $playGround.innerHTML += `
     <h2 class="prevent"> You ${byWinner} after playing ${total} rounds with ${tie} ties </h2>
   `;
 }
 
 
-
-btns.forEach(btn => {
-  btn.addEventListener('click', function(e){
-     playerSelection = e.target.textContent;
-     computerSelection = computerPlay();
-
-
+$btns.forEach($btn => {
+  $btn.addEventListener('click', function(e){
+    playerSelection = e.target.textContent;
+    computerSelection = computerPlay();
 
     playARound((playerSelection).toLowerCase(), computerSelection)
     updateScore();
